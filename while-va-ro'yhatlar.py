a9d92dfc7eabeb8825c566b2e57822be03a54b02
Xurshidbek Davronov <74503705+XurshidbekDavronov@users.noreply.github.com>
2021-07-09 18:14:11 +0500

    while-va-ro'yhatlar.py
    to'liq bajarildi

---
+++ while-va-ro'yhatlar.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  8 14:57:58 2021
+
+@author: admin
+"""
+
+# print('Do\'stlar ro\'hatini tuzish')
+# ismlar=[]
+# n=1
+# while True:
+#     savol=f"{n}-do'stingizning ismini kiriting: "
+#     ism=input(savol)
+#     ismlar.append(ism)
+#     takror=input('Yana ism qo\'shasizmi? (ha/yo\'q)')
+#     n+=1
+#     if takror!='ha':
+#        break
+#     else:
+#         continue
+
+# for ism in ismlar:
+#     print(ism.title())
+
+# print('Do\'stlarning yoshi haqida')
+# dostlar={}
+# ishora=True
+# while ishora:
+#     ism=input(f"Do'stingizning ismini kiriting: ")
+#     yosh=input(f"{ism.title()}ning yoshini kiriting: ")
+#     dostlar[ism]=int(yosh)
+#     javob=input('Yana ism qo\'shasizmi? (ha/yo\'q)')
+#     if javob=='yo\'q':
+#         ishora=False
+# for ism, yosh in dostlar.items():
+#     print(f"{ism.title()}ning yoshi {yosh}da")
+
+# cars=['nexia', 'lacetti', 'toyota', 'nexia', 'nexia', 'BMW']
+# car='nexia'
+# while car in cars:
+#     cars.remove(car)
+# print(cars)
+
+# oquvchilar=['olim','karim','sobir', 'hosil', 'zafar']
+# baholangan={}
+# print(oquvchilar)
+# while oquvchilar: #ro'yhat ichida birorta element qolmaguncha to'xtamaydi
+#     oquvchi=oquvchilar.pop() # ro'yhatning oxirgi elementini qirqib oladi
+#     baho=int(input(f"{oquvchi.title()}ning bahosini kiriting: "))
+#     print('O\'quvchi baholandi.')
+#     baholangan[oquvchi]=baho
+# print(baholangan)
+
+# print('Buyurtmalar ro\'hatini tuzish')
+# buyurtmalar=[]
+# n=1
+# while True:
+#     savol=f"{n}-buyutrmangizni kiriting: "
+#     buyurtma=input(savol)
+#     buyurtmalar.append(buyurtma)
+#     takror=input('Yana buyurtma qo\'shasizmi? (ha/yo\'q)')
+#     n+=1
+#     if takror!='ha':
+#         break
+#     else:
+#         continue
+
+# for buyurtma in buyurtmalar:
+#     print(buyurtma.title())
+# print(buyurtmalar)
+
